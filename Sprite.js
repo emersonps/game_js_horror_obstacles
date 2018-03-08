f66cb95979d0cfa4dd3dbe32e7b0105e37422ccc
Emerson Pinheiro <emersonline2007@gmail.com>
2018-03-08 10:20:04 -0400

    Criação de variáveis de chamadas de Sprites.

--- Sprite.js
+++ Sprite.js
@@ -12,9 +12,9 @@ function Sprite(x, y, largura, altura){
 var bg = new Sprite(0, 0, 600, 600);
 spriteBoneco = new Sprite(618,16, 86, 86);
 
-perdeu = new Sprite(603, 478, 397, 358),
-jogar = new Sprite(603, 127, 397, 347),
-novo = new Sprite(68, 721, 287, 93),
-spriteRecord = new Sprite(28, 879, 441, 95),
-spriteChao = new Sprite(0, 604, 600, 54),
+perdeu = new Sprite(603, 478, 397, 358);
+jogar = new Sprite(603, 127, 397, 347);
+novo = new Sprite(68, 721, 287, 93);
+spriteRecord = new Sprite(28, 879, 441, 95);
+spriteChao = new Sprite(0, 604, 600, 54);
 
