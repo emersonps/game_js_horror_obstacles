87d71c76b65c8c7a384f09bb792ba7dc338f57ac
Emerson Pinheiro <emersonline2007@gmail.com>
2018-03-08 00:34:45 -0400

    Criação de Sprites, Adição de Imagens. Substituição de objetos sólidos pelos Sprites.

---
+++ Sprite.js
@@ -0,0 +1,20 @@
+function Sprite(x, y, largura, altura){
+	this.x = x;
+	this.y = y;
+	this.largura = largura;
+	this.altura = altura;
+
+	this.desenha = function(xCanvas, yCanvas){
+		ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
+	}
+}
+
+var bg = new Sprite(0, 0, 600, 600);
+spriteBoneco = new Sprite(618,16, 86, 86);
+
+perdeu = new Sprite(603, 478, 397, 358),
+jogar = new Sprite(603, 127, 397, 347),
+novo = new Sprite(68, 721, 287, 93),
+spriteRecord = new Sprite(28, 879, 441, 95),
+spriteChao = new Sprite(0, 604, 600, 54),
+
